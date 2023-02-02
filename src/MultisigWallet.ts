d074bfab9e558e5438780345d776501de1999621
Gusarich <danielsedovzzz@gmail.com>
2023-02-02 23:49:38 +0300

    remove global client && fromAddress static method

--- src/MultisigWallet.ts
+++ src/MultisigWallet.ts
@@ -1,13 +1,9 @@
 import { TonClient } from "ton"
 import { keyPairFromSecretKey, sign } from "ton-crypto"
-import { Address, beginCell, Cell, contractAddress, Dictionary, external, Message, StateInit } from "ton-core"
+import { Address, beginCell, Cell, contractAddress, Dictionary, external, Message, Slice, StateInit } from "ton-core"
 import { Order } from "./Order"
 
 const MULTISIG_CODE = Cell.fromBase64('te6cckECKwEABBgAART/APSkE/S88sgLAQIBIAIDAgFIBAUE2vIgxwCOgzDbPOCDCNcYIPkBAdMH2zwiwAAToVNxePQOb6Hyn9s8VBq6+RDyoAb0BCD5AQHTH1EYuvKq0z9wUwHwCgHCCAGDCryx8mhTFYBA9A5voSCYDqQgwgryZw7f+COqH1NAufJhVCOjU04gIyEiAgLMBgcCASAMDQIBIAgJAgFmCgsAA9GEAiPymAvHoHN9CYbZ5S7Z4BPHohwhJQAtAKkItdJEqCTItdKlwLUAdAT8ArobBKAATwhbpEx4CBukTDgAdAg10rDAJrUAvALyFjPFszJ4HHXI8gBzxb0AMmACASAODwIBIBQVARW77ZbVA0cFUg2zyCoCAUgQEQIBIBITAXOxHXQgwjXGCD5AQHTB4IB1MTtQ9hTIHj0Dm+h8p/XC/9eMfkQ8qCuAfQEIW6TW3Ey4PkBWNs8AaQBgJwA9rtqA6ADoAPoCAXoCEfyAgPyA3XlP+AXkegAA54tkwAAXrhlXP8EA1WZ2oexAAgEgFhcCASAYGQFRtyVbZ4YmRmpGEAgegc30McJNhFpAADMaYeYuAFrgJhwLb+4cC3d0bhAjAYm1WZtnhqvgb+2xxsoicAgej430pBHEoFpAADHDhBACGuQkuuBk9kUWE5kAOeLKhACQCB6IYFImHFImHFImXEA2YlzNijAjAgEgGhsAF7UGtc4QQDVZnah7EAIBIBwdAgOZOB4fARGsGm2eL4G2CUAjABWt+UEAzJV2oewYQAENqTbPBVfBYCMAFa3f3CCAarM7UPYgAiDbPALyZfgAUENxQxPbPO1UIyoACtP/0wcwBKDbPC+uUyCw8mISsQKkJbNTHLmwJYEA4aojoCi8sPJpggGGoPgBBZcCERACPj4wjo0REB/bPEDXePRDEL0F4lQWW1Rz51YQU9zbPFRxClR6vCQlKCYAIO1E0NMf0wfTB9M/9AT0BNEAXgGOGjDSAAHyo9MH0wdQA9cBIPkBBfkBFbrypFAD4GwhIddKqgIi10m68qtwVCATAAwByMv/ywcE1ts87VT4D3AlblOJvrGYEG4QLVDHXwePGzBUJANQTds8UFWgRlAQSRA6SwlTuds8UFQWf+L4AAeDJaGOLCaAQPSWb6UglDBTA7neII4WODk5CNIAAZfTBzAW8AcFkTDifwgHBZJsMeKz5jAGKicoKQBgcI4pA9CDCNcY0wf0BDBTFnj0Dm+h8qXXC/9URUT5EPKmrlIgsVIDvRShI27mbCIyAH5SML6OIF8D+ACTItdKmALTB9QC+wAC6DJwyMoAQBSAQPRDAvAHjhdxyMsAFMsHEssHWM8BWM8WQBOAQPRDAeIBII6KEEUQNEMA2zztVJJfBuIqABzIyx/LB8sHyz/0APQAybmYlfQ=')
-
-const client = new TonClient({
-    endpoint: 'https://toncenter.com/api/v2/jsonRPC',
-})
 
 export class MultisigWallet {
     public publicKeys: Buffer[]
@@ -39,11 +35,23 @@ export class MultisigWallet {
         this.address = contractAddress(workchain, stateInit)
     }
     
-    static fromAddress (address: Address, client: TonClient) {
-        
+    static async fromAddress (client: TonClient, address: Address): Promise<MultisigWallet> {
+        const contractState = await client.getContractState(address)
+        const data: Slice = Cell.fromBoc(contractState.data!)[0].beginParse()
+        const walletId: number = data.loadUint(32)
+        data.skip(8)
+        const k: number = data.loadUint(8)
+        data.skip(64)
+        const owners = data.loadDict(Dictionary.Keys.Uint(8), Dictionary.Values.Cell())
+        let publicKeys: Buffer[] = []
+        for (const [key, value] of owners) {
+            const publicKey = value.beginParse().loadBuffer(64)
+            publicKeys.push(publicKey)
+        }
+        return new MultisigWallet(publicKeys, address.workChain, walletId, k)
     }
 
-    public sendOrder (order: Order, secretKey: Buffer) {
+    public sendOrder (client: TonClient, order: Order, secretKey: Buffer) {
         let sugoma = beginCell()
             .storeDict(order.signatures, Dictionary.Keys.Uint(8), Dictionary.Values.Buffer(64))
             .storeBuffer(order.messagesCell.hash())
