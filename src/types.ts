92e0edce15847aff83a3dd2a934aaf40897df576
Gusarich <danielsedovzzz@gmail.com>
2023-02-03 18:15:48 +0300

    first test for order class

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,6 @@
-import { WalletContractV1R1, WalletContractV1R2, WalletContractV1R3, WalletContractV2R1, WalletContractV2R2, WalletContractV3R1, WalletContractV3R2, WalletContractV4 } from "ton"
 import { Message } from "ton-core"
 
 export type MessageWithMode = {
     message: Message
     mode: number
 }
-
-export type WalletContract = WalletContractV1R1 | WalletContractV1R2 | WalletContractV1R3 | WalletContractV2R1 | WalletContractV2R2 | WalletContractV3R1 | WalletContractV3R2 | WalletContractV4
--- test/test.ts
+++ test/test.ts
@@ -1,7 +1,50 @@
 import { expect } from 'chai'
+import { beginCell, Cell, CurrencyCollection, Address, CommonMessageInfoInternal } from 'ton-core'
+import { testAddress } from 'ton-emulator'
+import { MessageWithMode } from '../src/types'
+import { Order, MultisigWallet } from './../src/index'
+
+function createCommonMessageInfoInternal (bounce: boolean, dest: Address, value: bigint): CommonMessageInfoInternal {
+    return {
+        bounce,
+        bounced: false,
+        createdAt: 0,
+        createdLt: 0n,
+        dest,
+        forwardFee: 0n,
+        ihrDisabled: true,
+        ihrFee: 0n,
+        src: Address.parse('EQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAM9c'),
+        type: 'internal',
+        value: {
+            coins: value
+        }
+    }
+}
+
+function createInternalMessageWithMode (bounce: boolean, dest: Address, value: bigint, body: Cell, mode: number = 3): MessageWithMode {
+    return {
+        message: {
+            info: createCommonMessageInfoInternal(bounce, dest, value),
+            body
+        },
+        mode: mode
+    }
+}
 
 describe('Order', () => {
-    
+    let order: Order
+
+    beforeEach(() => {
+        order = new Order()
+    })
+
+    it ('should add messages', () => {
+        order.addMessage(createInternalMessageWithMode(true, testAddress('address1'), 1000000000n, Cell.EMPTY))
+        order.addMessage(createInternalMessageWithMode(true, testAddress('address2'), 0n, beginCell().storeUint(3, 123).endCell()))
+        order.addMessage(createInternalMessageWithMode(true, testAddress('address1'), 2000000000n, Cell.EMPTY))
+        expect(order.messages).to.have.lengthOf(3)
+    })
 })
 
 describe('MultisigWallet', () => {
